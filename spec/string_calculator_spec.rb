2d9584e4dff7893b397a6e50c6a89c5fd15c5bb8
amd4k <ameed.cdac@gmail.com>
2025-10-18 03:31:42 +0530

    Add method: A single number returns itself. Completed the second Red-Green cycle of the TDD

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe StringCalculator do
            expect(calculator.add('')).to eq(0)
         end
     end
+
+    context 'when a single number is provided' do
+        it 'returns the nummber itself' do
+           calculator = StringCalculator.new
+           expect(calculator.add('1')).to eq(1)
+        end
+    end
   end
 end
