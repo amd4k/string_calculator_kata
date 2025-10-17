58789d839aa7d4296fe31423e9c8b2507d6c5ecc
amd4k <ameed.cdac@gmail.com>
2025-10-18 02:09:27 +0530

    Add method: Empty string returns 0. Completed the first Red-Green cycle of the TDD

---
+++ spec/string_calculator_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+require_relative '../lib/string_calculator'
+RSpec.describe StringCalculator do
+  describe '#add' do
+    context 'when given an empty string' do
+        it ' returns 0' do
+           calculator = StringCalculator.new
+           expect(calculator.add('')).to eq(0)
+        end
+    end
+  end
+end
