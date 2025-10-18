c4790857972a7c03d5fff45dd040fef0b79bd609
amd4k <ameed.cdac@gmail.com>
2025-10-19 00:50:45 +0530

    Add method: added custom delimiter support

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,16 +1,23 @@
 class StringCalculator
     def add(numbers)
         return 0 if numbers.empty?
-
+        return custom_delimiter(numbers).sum if numbers.start_with?("//")
         parse_num(numbers).sum
-
     end
 
     private
     def parse_num(numbers)
         normalized_numbers = numbers.gsub("\n", ",")
         num_arr = normalized_numbers.split(',')
-        p num_arr
+        num_arr.map(&:to_i)
+
+    end
+
+    def custom_delimiter(numbers) 
+        header, body = numbers.split("\n", 2) if numbers.start_with?("//")
+        delimiter = header[2] if header
+        num_arr = body.split(/#{Regexp.escape(delimiter)}|,|\n/)
         num_arr.map(&:to_i)
     end
+
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,38 +2,45 @@ require 'spec_helper'
 require_relative '../lib/string_calculator'
 RSpec.describe StringCalculator do
   describe '#add' do
+    let(:calculator) { StringCalculator.new }
+
     context 'when given an empty string' do
-        it ' returns 0' do
-           calculator = StringCalculator.new
+        it 'returns 0' do
            expect(calculator.add('')).to eq(0)
         end
     end
 
     context 'when a single number is provided' do
-        it 'returns the nummber itself' do
-           calculator = StringCalculator.new
+        it 'returns the numbers itself' do
            expect(calculator.add('1')).to eq(1)
         end
     end
 
     context 'when two numbers are provided' do
         it 'returns the sum of the two numbers' do
-           calculator = StringCalculator.new
            expect(calculator.add('1,2')).to eq(3)
         end
     end
 
     context 'when multiple numbers are provided' do
         it 'returns the sum of all the numbers' do
-           calculator = StringCalculator.new
            expect(calculator.add('1,2,3,4,5')).to eq(15)
         end
     end
 
-    context 'when numbers are separated by new lines' do
+    context 'when numbers are separated by newlines' do
         it 'returns the sum of all the numbers' do
-           calculator = StringCalculator.new
            expect(calculator.add("1\n2,3")).to eq(6)
+        end
+    end
+
+    context 'when custom delimiters are used' do
+        it 'supports semicolon as a delimiter' do
+            expect(calculator.add("//;\n1;2")).to eq(3)
+        end
+
+        it 'supports asterisk as a delimiter' do
+            expect(calculator.add("//*\n1*2,5\n3,7,9*400,0*5")).to eq(432)
         end
     end
 
